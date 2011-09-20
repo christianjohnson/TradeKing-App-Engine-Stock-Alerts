f0c205cf403032498e435861a577f27274947ffc
jewishdan18 <jewishdan18@gmail.com>
2011-09-20 14:19:49 -0700

    Initial main files

---
+++ main.py
@@ -0,0 +1,68 @@
+from google.appengine.ext import webapp
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import users
+import logging
+import cgi, os, urllib
+
+class Alert(db.Model):
+  ticker    = db.StringProperty()
+  hi_price  = db.FloatProperty()
+  low_price = db.FloatProperty()
+  user      = db.UserProperty()
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write("Hello!")
+
+class CheckAlerts(webapp.RequestHandler):
+  def get(self):
+    self.response.out.write("Hi!")
+
+class AddAlert(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+    
+    user = users.get_current_user()
+
+    if not user:
+      self.response.out.write("no user found, redirecting to login...")
+      self.redirect(users.create_login_url(self.request.uri))
+    else:      
+      ticker    = urllib.unquote( cgi.escape(self.request.get('ticker'   )).lower() )
+      hi_price  = urllib.unquote( cgi.escape(self.request.get('hi_price' )).lower() )
+      low_price = urllib.unquote( cgi.escape(self.request.get('low_price')).lower() )
+      
+      query = Alert.all()
+      query.filter('user = '  , user)
+      query.filter('ticker = ', ticker)
+      
+      prev_alerts = query.fetch(1)
+      
+      if prev_alerts:
+        prev_alert = prev_alerts[0]
+        
+        prev_alert.hi_price  = float(hi_price)
+        prev_alert.low_price = float(low_price)
+        prev_alert.put()
+      else:
+        alert = Alert()
+        
+        alert.ticker    = ticker
+        alert.hi_price  = float(hi_price)
+        alert.low_price = float(low_price)
+        alert.user      = user
+        
+        alert.put()
+
+application = webapp.WSGIApplication(
+  [('/check_alerts', CheckAlerts),
+   ('/add_alert', AddAlert),
+   ('/', MainPage),])
+   
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
